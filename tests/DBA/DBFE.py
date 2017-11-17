aa62cbd0636211e37136a75d06ac23ea4bb1e406
Dan Maljovec <maljovec002@gmail.com>
2017-11-16 21:54:28 -0700

    Applying pylint suggested changes and using an RBF for the DBFE_SVM algorithm in order to have a better fit on the BWR dataset. Otherwise, without the maxCladTemp column, the SVM decision boundary was degenerate.

--- tests/DBA/DBFE.py
+++ tests/DBA/DBFE.py
@@ -1,15 +1,57 @@
+"""
+    Decision Boundary Feature Extraction module
+"""
+# pylint: disable=C0103
+# pylint: disable=R0902
+
 import numpy as np
 from sklearn.svm import SVC, NuSVC
 from sklearn.model_selection import GridSearchCV
+from sklearn import cluster
 import numdifftools as nd
 import ngl
 import time
 from scipy.optimize import bisect, fsolve
 import sys
+from scipy.spatial import distance
 
 from sklearn.neighbors import NearestNeighbors
 
+def FindZero(X0, X1, func, eps):
+    """
+        Find the zero existing between two input values given a function
+        func and a tolerance eps
+    """
+    # stepSize = 1e-3
+    # alpha = 0.5
+    y0 = func(X0)
+    y1 = func(X1)
+    # minStepSize = 1e-16
+    # maxCount = 10000
+
+    def f(alpha):
+        """
+            Parametric function to allow us to vary between X0 and X1
+        """
+        return func(alpha*X0 + (1-alpha)*X1)
+
+    ## We cannot interpolate here because the SVM is misclassifying
+    ## one of these values, so we will return a None and have the calling
+    ## environment handle this appropriately.
+    if np.sign(y0) == np.sign(y1):
+        return None
+
+    alpha = bisect(f, 0, 1, xtol=eps)
+    # alpha = fsolve(f, 0.5)
+    if alpha >= 0 and alpha <= 1:
+        return alpha*X0 + (1-alpha)*X1
+    else:
+        return None
+
 class DBFE(object):
+    """
+        Decision Boundary Feature Extraction Class
+    """
     def __init__(self, X, Y):
         self.X = X
         self.Y = Y
@@ -67,67 +109,47 @@ class DBFE(object):
         print(' {:5.2f} s'.format(end-start))
         sys.stdout.flush()
 
-    def FindZero(self, X0, X1, func, eps):
-        # stepSize = 1e-3
-        # alpha = 0.5
-        y0 = func(X0)
-        y1 = func(X1)
-        # minStepSize = 1e-16
-        # maxCount = 10000
-
-        def foo(alpha):
-            return func(alpha*X0 + (1-alpha)*X1)
-
-        ## We cannot interpolate here because the SVM is misclassifying
-        ## one of these values, so we will return a None and have the calling
-        ## environment handle this appropriately.
-        if np.sign(y0) == np.sign(y1):
-            return None
-
-        alpha = bisect(foo, 0, 1, xtol=eps)
-        # alpha = fsolve(foo, 0.5)
-        if 0 <= alpha and alpha <= 1:
-            return alpha*X0 + (1-alpha)*X1
-        else:
-            return None
-
     def BuildModel(self):
         """
             By default, we will use a random decision identifier and everything
             will be assumed to be on the boundary. This should be reimplemented
             by each subclass
         """
-        self.decision = lambda x: (1 - 2*np.random.randint(0,2))
+        self.decision = lambda x: (1 - 2*np.random.randint(0, 2))
         self.indices = list(range(len(self.Y)))
 
     def BuildGraph(self, graphType='beta skeleton', k=10, beta=1.0):
+        """
+            Build a graph on the data of this object and store it internally
+        """
         nn = NearestNeighbors(k, n_jobs=-1).fit(self.Xs)
         pairs = []
         for i in range(len(self.Xs)):
-            neighborIndices = nn.kneighbors(self.Xs[i].reshape(1,-1), return_distance=False).flatten()
+            Xs = self.Xs[i].reshape(1, -1)
+            neighborIndices = nn.kneighbors(Xs, return_distance=False).flatten()
             for j in neighborIndices:
                 if i != j:
-                    pairs.append((i,j))
+                    pairs.append((i, j))
 
-        # As seen here:
-        #  http://stackoverflow.com/questions/480214/how-do-you-remove-duplicates-from-a-list-in-python-whilst-preserving-order
+        ## As seen here:
+        ##  https://tinyurl.com/angxm5
         seen = set()
-        pairs = [ x for x in pairs if not (x in seen or x[::-1] in seen
-                                           or seen.add(x))]
+        pairs = [x for x in pairs if not (x in seen or x[::-1] in seen or seen.add(x))]
+
         edgesToPrune = []
         for edge in pairs:
-          edgesToPrune.append(int(edge[0]))
-          edgesToPrune.append(int(edge[1]))
+            edgesToPrune.append(int(edge[0]))
+            edgesToPrune.append(int(edge[1]))
 
-        a = ngl.ngl.vectorInt([1,2])
+        a = ngl.ngl.vectorInt([1, 2])
         ngl.ngl.vectorInt(edgesToPrune)
         self.graph = ngl.Graph(self.Xs, graphType, k, beta, edgesToPrune)
 
     def ExtractSurfacePoints(self, r=0.2):
         """
-            This function will determine return the surface points
+            This function will determine and return the surface points
         """
-        count  = int(min(r*len(self.Y), len(self.indices)))
+        count = int(min(r*len(self.Y), len(self.indices)))
         sortedIndices = []
 
         for i in self.indices:
@@ -135,7 +157,7 @@ class DBFE(object):
                 proximitiesToBoundaries = np.abs(self.decision(self.Xs[i]) - self.decisionOffset)
                 ## Figure out which class this support vector is determining
                 decisionIdx = np.argmin(proximitiesToBoundaries)
-                sortedIndices.append((proximitiesToBoundaries[:,decisionIdx], i, decisionIdx))
+                sortedIndices.append((proximitiesToBoundaries[:, decisionIdx], i, decisionIdx))
             else:
                 ## We don't need a decisionIdx here or a list, but setting them
                 ## makes it look like the multiclass case
@@ -146,12 +168,12 @@ class DBFE(object):
         ## Start with the indices closest to the boundaries, note we don't need
         ## the results of the decision function after this, only the sorted
         ## order of indices, and the decisionIdx (for the multiclass case)
-        sortedIndices = [ (i,d) for (v,i,d) in sorted(sortedIndices) ]
+        sortedIndices = [(i, d) for (v, i, d) in sorted(sortedIndices)]
 
         S = []
         decisionIndices = []
         counter = 0
-        for i,decisionIdx in sortedIndices[:count]:
+        for i, decisionIdx in sortedIndices[:count]:
             start = time.time()
             neighborIndices = self.graph.Neighbors(i)
             # neighborIndices = self.graph.kneighbors(self.Xs[i], return_distance=False)
@@ -167,10 +189,11 @@ class DBFE(object):
                     ## function, for multiclass we have to appropriately index
                     ## the correct decision function
                     if self.isMultiClass:
-                        decisionFunction = lambda x: self.decision(x)[:,decisionIdx] - self.decisionOffset
+                        offset = self.decisionOffset
+                        decisionFunction = lambda x: self.decision(x)[:, decisionIdx] - offset
                     else:
                         decisionFunction = lambda x: self.decision(x)
-                    surfacePoint = self.FindZero(self.Xs[i], self.Xs[j], decisionFunction, 1e-3)
+                    surfacePoint = FindZero(self.Xs[i], self.Xs[j], decisionFunction, 1e-3)
                     if surfacePoint is not None:
                         S.append(surfacePoint)
                         ## For multiclass
@@ -184,9 +207,10 @@ class DBFE(object):
     def ComputeNumericGradients(self):
         grads = np.zeros(self.S.shape)
 
-        for i,(s,decisionIdx) in enumerate(zip(self.S,self.decisionIndices)):
+        for i, (s, decisionIdx) in enumerate(zip(self.S, self.decisionIndices)):
             if self.isMultiClass:
-                decisionFunction = lambda x: self.decision(x)[:,decisionIdx] - (len(self.classes)-1)
+                offset = len(self.classes)-1
+                decisionFunction = lambda x: self.decision(x)[:, decisionIdx] - offset
             else:
                 decisionFunction = lambda x: self.decision(x)
             N = nd.Gradient(decisionFunction)
@@ -205,36 +229,38 @@ class DBFE(object):
         ## Find the two closest classes for each support vector to determine
         ## which boundary it represents in a one-vs-one setting
         boundaries = np.zeros((len(self.model.support_vectors_), 2), dtype=int)
-        for j,xj in enumerate(self.model.support_vectors_):
+        for j, xj in enumerate(self.model.support_vectors_):
 
             ## Find out the two closest classes
             if k > 2:
-                a,b = np.argsort(np.abs(self.model.decision_function(xj.reshape(1,-1)) - (k-1)))[0,:2]
+                diffs = np.abs(self.model.decision_function(xj.reshape(1, -1)) - (k-1))
+                a, b = np.argsort(diffs)[0, :2]
                 if a > b:
-                    a,b = b,a
+                    a, b = b, a
             else:
-                a,b = 0,1
-            boundaries[j] = [a,b]
+                a, b = 0, 1
+            boundaries[j] = [a, b]
 
-        for i,s in enumerate(self.S):
+        for i, s in enumerate(self.S):
             start = time.time()
             if k > 2:
-                a,b = np.argsort(np.abs(self.model.decision_function(s.reshape(1,-1)) - (k-1)))[0,:2]
+                diffs = np.abs(self.model.decision_function(s.reshape(1, -1)) - (k-1))
+                a, b = np.argsort(diffs)[0, :2]
                 if a > b:
-                    a,b = b,a
+                    a, b = b, a
             else:
-                a,b = 0,1
+                a, b = 0, 1
 
-            ovo = (a,b)
+            ovo = (a, b)
             decisionIdx = b-1
 
             summand = np.zeros(len(s))
             sumWeights = 0
-            for j,xj in enumerate(self.model.support_vectors_):
+            for j, xj in enumerate(self.model.support_vectors_):
                 if ovo != tuple(boundaries[j]):
                     continue
 
-                numerator = self.model.dual_coef_[decisionIdx,j] * (np.dot(s, xj))**p
+                numerator = self.model.dual_coef_[decisionIdx, j] * (np.dot(s, xj))**p
                 denominator = np.dot(s, xj)
                 coefficient = numerator/denominator
                 sumWeights += coefficient
@@ -259,7 +285,7 @@ class DBFE(object):
         # print('\t\tAnalytic Gradients: {} s'.format(end-start))
 
         ## Normalize the normals
-        gradMags = np.linalg.norm(grads,axis=1).reshape(-1,1)
+        gradMags = np.linalg.norm(grads, axis=1).reshape(-1, 1)
 
         ## Remove degenerate normals (where did these come from? Could be an
         ## important question)
@@ -273,29 +299,32 @@ class DBFE(object):
 
         # start = time.time()
         ## Construct the Decision Boundary Scatter Matrix (DBSM)
-        DBSM = np.zeros((Ni.shape[1],Ni.shape[1]))
+        DBSM = np.zeros((Ni.shape[1], Ni.shape[1]))
         for ni in Ni:
-            DBSM += np.outer(ni,ni)
+            DBSM += np.outer(ni, ni)
         DBSM /= float(len(Ni))
 
         # end = time.time()
         # print('\t\tDBSM: {} s'.format(end-start))
 
         # start = time.time()
-        values,vectors = np.linalg.eig(DBSM)
+        values, vectors = np.linalg.eig(DBSM)
         # end = time.time()
         # print('\t\tEigendecomposition: {} s'.format(end-start))
 
         features = []
-        for value,vector in zip(values,vectors):
+        for value, vector in zip(values, vectors):
             if value != 0:
                 features.append(vector)
 
         self.features = np.array(features)
 
     def transform(self, X, maxDimensionality=2):
-        maxDimensionality = min(self.features.shape[0],maxDimensionality)
+        maxDimensionality = min(self.features.shape[0], maxDimensionality)
         return self.features[:maxDimensionality].dot(X)
+
+    def writeTransform(self, baseFileName=''):
+        pass
 
 class DBFE_LVQ(DBFE):
     pass
@@ -304,26 +333,89 @@ class DBFE_KNN(DBFE):
     pass
 
 class DBFE_SVM(DBFE):
-    def __init__(self, X, Y, C=1, n_folds=0, tuned_parameters = {}, kernel='poly', degree=2):
+    def __init__(self, X, Y, C=1, n_folds=0, tuned_parameters={}, kernel='poly', degree=2):
         self.C = 1
         self.degree = degree
         self.kernel = kernel
         self.cv_parameters = tuned_parameters
-        self.n_folds= n_folds
+        self.n_folds = n_folds
 
         super(DBFE_SVM, self).__init__(X, Y)
 
 
     def BuildModel(self):
         ## Train a support vector machine
-        svc = SVC(C=self.C, kernel=self.kernel, degree=self.degree, gamma='auto', shrinking=True, tol=1e-3, cache_size=8000, class_weight=None, max_iter=1e6)
+        svc = SVC(C=self.C, kernel=self.kernel, degree=self.degree, gamma='auto',
+                  shrinking=True, tol=1e-3, cache_size=8000, class_weight=None,
+                  max_iter=1e6)
         if self.n_folds > 0 and len(self.cv_parameters.keys()) > 0:
-            self.model = GridSearchCV(svc, self.cv_parameters, cv = self.n_folds, refit=True).estimator
+            cv = self.n_folds
+            self.model = GridSearchCV(svc, self.cv_parameters, cv=cv, refit=True).estimator
         else:
             self.model = svc
-        # self.model = NuSVC(nu=0.5, kernel='rbf', gamma='auto', shrinking=True, tol=0.001, cache_size=8000, class_weight=None, max_iter=-1)
+        ## self.model = NuSVC(nu=0.5, kernel='rbf', gamma='auto', shrinking=True,
+        ## tol=0.001, cache_size=8000, class_weight=None, max_iter=-1)
         _ = self.model.fit(self.Xs, self.Y)
         self.decision = lambda x: self.model.decision_function(x.reshape(1, -1))
 
         ## Get the support vectors
         self.indices = self.model.support_
+
+class DBFE_Clustered_SVM(DBFE_SVM):
+    def FeatureExtraction(self):
+        grads = self.ComputeAnalyticGradients()
+        np.savetxt('analyticGrads.csv', grads, delimiter=',')
+
+        ## Normalize the normals
+        gradMags = np.linalg.norm(grads, axis=1).reshape(-1, 1)
+
+        ## Remove degenerate normals (where did these come from? Could be an
+        ## important question)
+        idxsToKeep = np.nonzero(gradMags)[0]
+
+        Ni = grads[idxsToKeep]/gradMags[idxsToKeep]
+
+        ## Modification 3:
+        ## Partition the data at this point
+        ## TODO: define criteria for separating the data
+        X = np.hstack((self.S[idxsToKeep], Ni))
+        connectivity = np.zeros(X.shape[0], X.shape[0])
+
+        d = self.S.shape[1]
+        for i in enumerate(X):
+            xi = X[i]
+            for j in enumerate(X):
+                if i == j:
+                    continue
+                xj = X[j]
+                euclidean = np.linalg.norm(xi[:d]-xj[:d])
+                cosine = distance.cosine(xi[d:], xj[d:])
+
+
+        clustering = sklearn.cluster.AgglomerativeClustering(n_clusters=10,
+                                                             affinity='precomputed',
+                                                             connectivity=connectivity,
+                                                             linkage='average')
+        labels = clustering.fit_predict(X)
+
+        # start = time.time()
+        ## Construct the Decision Boundary Scatter Matrix (DBSM)
+        DBSM = np.zeros((Ni.shape[1], Ni.shape[1]))
+        for ni in Ni:
+            DBSM += np.outer(ni, ni)
+        DBSM /= float(len(Ni))
+
+        # end = time.time()
+        # print('\t\tDBSM: {} s'.format(end-start))
+
+        # start = time.time()
+        values, vectors = np.linalg.eig(DBSM)
+        # end = time.time()
+        # print('\t\tEigendecomposition: {} s'.format(end-start))
+
+        features = []
+        for value, vector in zip(values, vectors):
+            if value != 0:
+                features.append(vector)
+
+        self.features = np.array(features)
--- tests/DBA/analyzeBWR.py
+++ tests/DBA/analyzeBWR.py
@@ -1,18 +1,24 @@
+"""
+    Test module for the BWR dataset
+"""
+# pylint: disable=C0103
+
+import time
+
 from DBFE import DBFE_SVM
 import pandas as pd
 import numpy as np
-import time
 from sklearn import preprocessing
-from sklearn.model_selection import KFold
+
 from sklearn.model_selection import GridSearchCV
 from sklearn.svm import SVC
 from sklearn.decomposition import PCA
 # from sklearn.discriminant_analysis import LinearDiscriminantAnalysis as LDA
 from PseudoLDA import PLDA
 
-import matplotlib.pyplot as plt
-import seaborn as sns
-import pandas as pd
+# import matplotlib.pyplot as plt
+# import seaborn as sns
+
 
 filename = 'data/BWR_SBO.csv'
 dataset = pd.read_csv(filename).as_matrix()
@@ -20,14 +26,14 @@ print("File read")
 
 
 n_folds = 12
-tuned_parameters = {'C': np.linspace(1,1000,100)}
+tuned_parameters = {'C': np.linspace(1, 1000, 100)}
 
 svc = SVC(C=2, kernel='rbf', gamma='auto',
           tol=1e-3, cache_size=8000, max_iter=-1)
-svc_cv = GridSearchCV(svc, tuned_parameters, cv = n_folds, refit=True)
+svc_cv = GridSearchCV(svc, tuned_parameters, cv=n_folds, refit=True)
 
-X = dataset[:,:-2]
-Y = dataset[:,-1]
+X = dataset[:, :-2]
+Y = dataset[:, -1]
 
 scaler = preprocessing.MinMaxScaler()
 scaler.fit(X)
@@ -35,27 +41,29 @@ X = scaler.transform(X)
 
 test = []
 for i in range(10):
-    test += range(i*200,i*200+50)
+    test += range(i*200, i*200+50)
+
 train = np.array(list(set(range(2000))-set(test)))
 test = np.array(test)
 
-for d in range(1,12):
+for d in range(1, 12):
     for method in ['LDA','PCA','DBA']:
 
-        print(d,method)
+        print(d, method)
 
 
         start = time.time()
         if method == 'DBA':
-            model = DBFE_SVM(X[train],Y[train], C=2,
-                             kernel='poly', degree=5)
-            train_X = model.transform(X[train].T,d).T
-            test_X = model.transform(X[test].T,d).T
+            model = DBFE_SVM(X[train], Y[train], C=1000, kernel='rbf', degree=5)
+            train_X = model.transform(X[train].T, d).T
+            test_X = model.transform(X[test].T, d).T
         else:
             if method == 'PCA':
-                model = PCA(n_components=d).fit(X[train],Y[train])
+                model = PCA(n_components=d).fit(X[train], Y[train])
+                np.savetxt('bwr_{}_{}'.format(method, d), model.transform(np.eye(X.shape[1])))
             else:
-                model = PLDA(n_components=d).fit(X[train],Y[train])
+                model = PLDA(n_components=d).fit(X[train], Y[train])
+                np.savetxt('bwr_{}_{}'.format(method, d), model.transformMatrix)
             train_X = model.transform(X[train])
             test_X = model.transform(X[test])
 
@@ -63,15 +71,16 @@ for d in range(1,12):
         print('\tTransform Data: {:5.2f} s'.format(end-start))
         start = time.time()
 
-        try:
-            cols = ['x{}'.format(i) for i in range(train_X.shape[1])]
-            df = pd.DataFrame(np.hstack((train_X, np.atleast_2d(Y[train]).T)), columns= cols + ['y'])
-            sns.pairplot(df, hue='y', vars=cols)
-        except ValueError:
-            pass
+        # try:
+        #     cols = ['x{}'.format(i) for i in range(train_X.shape[1])]
+        #     df = pd.DataFrame(np.hstack((train_X, np.atleast_2d(Y[train]).T)),
+        #                       columns= cols + ['y'])
+        #     sns.pairplot(df, hue='y', vars=cols)
+        # except ValueError:
+        #     pass
 
-        plt.savefig('bwr_{}_{}D.png'.format(method, d))
-        plt.close()
+        # plt.savefig('bwr_{}_{}D.png'.format(method, d))
+        # plt.close()
 
         test_model = svc_cv.fit(train_X, Y[train])
 
